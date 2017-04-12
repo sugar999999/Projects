3d505f312ac70170faeb8086b1cc1e69dc1324b7
Sugar999 <sugar_maestro@yahoo.co.jp>
2017-04-13 00:25:41 +0900

    Update ConsoleApplication1.cpp

--- app1/ConsoleApplication1.cpp
+++ app1/ConsoleApplication1.cpp
@@ -1,59 +1,93 @@
-// ConsoleApplication1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
-
 
 #include "stdafx.h"
 
-using namespace System;
-using namespace System::IO;
+
+int main(){
+
+	PMIB_TCPTABLE_OWNER_PID pTcpTable = NULL;
+	PMIB_UDPTABLE_OWNER_PID pUdpTable = NULL;
+	DWORD dwSize = 0;
+	BOOL bOrder = TRUE;
+	ULONG ulAf = AF_INET;
+	MIB_TCPROW_OWNER_PID rowTcp;
+	MIB_UDPROW_OWNER_PID rowUdp;
+	DWORD dwRet;
 
 
+	dwRet = GetExtendedTcpTable(NULL, &dwSize, bOrder, ulAf, TCP_TABLE_OWNER_PID_ALL, 0);
+
+	pTcpTable = (PMIB_TCPTABLE_OWNER_PID)malloc(dwSize);
+
+	dwRet = GetExtendedTcpTable(pTcpTable, &dwSize, bOrder, ulAf, TCP_TABLE_OWNER_PID_ALL, 0);
+
+	printf("%-6s %-23s %-23s %6s\n",
+
+		"繝励Ο繝医さ繝ｫ",
+		"繝ｭ繝ｼ繧ｫ繝ｫ 繧｢繝峨Ξ繧ｹ",
+		"螟夜Κ繧｢繝峨Ξ繧ｹ",
+		"PID");
 
 
-int main()
-{	
-	size_t testCount = 0;
-	TCHAR szBufM[256];
-	TCHAR szBufP[256];
-	HANDLE hSnapProc;
-	HANDLE hSnapMod;
-	MODULEENTRY32 me;
-	PROCESSENTRY32 pe;
-	hSnapProc = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (hSnapProc == INVALID_HANDLE_VALUE) Console::WriteLine("N/A");
-	
-
-	me.dwSize = sizeof(MODULEENTRY32);
-	pe.dwSize = sizeof(PROCESSENTRY32);
-	Process32First(hSnapProc, &pe);
-	
-	do {
-		wsprintf(szBufP, TEXT("%s\t%d"), pe.szExeFile, pe.th32ProcessID);
-		for (testCount = 0; testCount < wcslen(szBufP); testCount++)
-			std::cout << static_cast<char>(szBufP[testCount]);
-		std::cout << std::endl;
-		hSnapMod = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pe.th32ProcessID);
-		if (hSnapMod == INVALID_HANDLE_VALUE) {
-			std::cout << "N/A" << std::endl << std::endl;
-			continue;
-		}
-		else {
-			Module32First(hSnapMod, &me);
-		}
-		do {
-			wsprintf(szBufM, TEXT("%s"), me.szModule);
-			for (testCount = 0; testCount < wcslen(szBufM); testCount++)
-				std::cout << static_cast<char>(szBufM[testCount]);
-			std::cout << ' ';
-		} while (Module32Next(hSnapMod, &me));
+	for (DWORD i = 0; i < pTcpTable->dwNumEntries; i++) {
 		
-		std::cout << std::endl << std::endl;
-	} while (Process32Next(hSnapProc, &pe));
+		
 
-	CloseHandle(hSnapProc);
-	CloseHandle(hSnapMod);
+		rowTcp = pTcpTable->table[i];
+
+		char bufLocalAddr[16];
+		char bufRemoteAddr[16];
+		char lpszHostName[50] = "";
+		char lpszServtName[50] = "";
+
+		inet_ntop(ulAf, &rowTcp.dwLocalAddr, bufLocalAddr, 16);
+		inet_ntop(ulAf, &rowTcp.dwRemoteAddr, bufRemoteAddr, 16);
+
+		int rc = getnameinfo((struct sockaddr *)bufRemoteAddr, 4, lpszHostName, 50, lpszServtName, 50, 0);
+
+		printf("%-6s %15s:%-8d %15s:%-8d %-6u\n",
+
+			"TCP",
+			bufLocalAddr,
+			ntohs((u_short)(rowTcp.dwLocalPort)),
+
+			lpszServtName, //bufRemoteAddr
+			ntohs((u_short)(rowTcp.dwRemotePort)),
+			rowTcp.dwOwningPid);
+	}
+	
+	free(pTcpTable);
+
+	
+	
+	dwRet = GetExtendedUdpTable(NULL, &dwSize, bOrder, ulAf, UDP_TABLE_OWNER_PID, 0);
+
+	pUdpTable = (PMIB_UDPTABLE_OWNER_PID)malloc(dwSize);
+
+	dwRet = GetExtendedUdpTable(pUdpTable, &dwSize, bOrder, ulAf, UDP_TABLE_OWNER_PID, 0);
+
+	for (DWORD i = 0; i < pUdpTable->dwNumEntries; i++) {
+
+		rowUdp = pUdpTable->table[i];
+
+
+		char bufLocalAddr[16];
+		inet_ntop(ulAf, &rowUdp.dwLocalAddr, bufLocalAddr, 16);
+
+
+		printf("%-6s %15s:%-8d %15c:%-8c %-6u\n",
+
+			"UDP",
+			bufLocalAddr,
+			ntohs((u_short)(rowUdp.dwLocalPort)),
+
+			'*',
+			'*',
+			rowUdp.dwOwningPid);
+	}
+
+	free(pUdpTable);
+
 
 	getchar();
-
 	return 0;
 }
