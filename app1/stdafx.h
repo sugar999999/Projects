51660609d2bb6f5c11399d4f1370eea5c8bb81c7
Sugar999 <sugar_maestro@yahoo.co.jp>
2017-04-16 22:45:39 +0900

    Update stdafx.h

--- app1/stdafx.h
+++ app1/stdafx.h
@@ -1,22 +1,137 @@
-// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
-// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
-// を記述します。
-//
-
 #pragma once
 
-#include "targetver.h"
+#pragma region Includes 
+#include <stdio.h> 
+#include <tchar.h> 
+#include <windows.h> 
+#include <psapi.h> 
+#include <strsafe.h> 
+#include <assert.h> 
+#include <tlhelp32.h>
 
-#include <stdio.h>
-#include <tchar.h>
-#include <windows.h>
-#include <Winternl.h>
-#include <vector>
-#include <algorithm>
-#include <ntstatus.h>
-#include <iterator>
+#include <memory>
 
-#pragma comment(lib, "Ntdll.lib")
+#pragma comment(lib, "ntdll.Lib")
+#pragma endregion 
 
 
-// TODO: プログラムに必要な追加ヘッダーをここで参照してください
+#define BUFFER_SIZE            512 
+
+
+#pragma region EnumerateFileHandles 
+
+#define STATUS_SUCCESS                    ((NTSTATUS)0x00000000L) 
+#define STATUS_INFO_LENGTH_MISMATCH        ((NTSTATUS)0xc0000004L) 
+
+
+#include <winternl.h> 
+
+// 
+const SYSTEM_INFORMATION_CLASS SystemHandleInformation =
+(SYSTEM_INFORMATION_CLASS)16;
+const OBJECT_INFORMATION_CLASS ObjectNameInformation = (OBJECT_INFORMATION_CLASS)1;
+
+// 
+// 
+// 
+// 
+typedef NTSTATUS(WINAPI * PFN_NTQUERYSYSTEMINFORMATION)(
+	IN SYSTEM_INFORMATION_CLASS SystemInformationClass,
+	OUT PVOID SystemInformation,
+	IN ULONG SystemInformationLength,
+	OUT PULONG ReturnLength OPTIONAL
+	);
+
+
+#define HANDLE_TYPE_FILE                28 
+
+// 
+typedef struct _SYSTEM_HANDLE
+{
+	ULONG ProcessId;
+	UCHAR ObjectTypeNumber;
+	UCHAR Flags;
+	USHORT Handle;
+	PVOID Object;
+	ACCESS_MASK GrantedAccess;
+} SYSTEM_HANDLE, *PSYSTEM_HANDLE;
+
+typedef struct _SYSTEM_HANDLE_INFORMATION
+{
+	ULONG NumberOfHandles;
+	SYSTEM_HANDLE Handles[1];
+} SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;
+
+
+typedef NTSTATUS(NTAPI * PFN_NTQUERYOBJECT)(
+	HANDLE ObjectHandle,
+	ULONG ObjectInformationClass,
+	PVOID ObjectInformation,
+	ULONG ObjectInformationLength,
+	PULONG ReturnLength
+	);
+
+typedef enum _POOL_TYPE
+{
+	NonPagedPool,
+	PagedPool,
+	NonPagedPoolMustSucceed,
+	DontUseThisType,
+	NonPagedPoolCacheAligned,
+	PagedPoolCacheAligned,
+	NonPagedPoolCacheAlignedMustS
+} POOL_TYPE, *PPOOL_TYPE;
+
+typedef struct _OBJECT_TYPE_INFORMATION
+{
+	UNICODE_STRING Name;
+	ULONG TotalNumberOfObjects;
+	ULONG TotalNumberOfHandles;
+	ULONG TotalPagedPoolUsage;
+	ULONG TotalNonPagedPoolUsage;
+	ULONG TotalNamePoolUsage;
+	ULONG TotalHandleTableUsage;
+	ULONG HighWaterNumberOfObjects;
+	ULONG HighWaterNumberOfHandles;
+	ULONG HighWaterPagedPoolUsage;
+	ULONG HighWaterNonPagedPoolUsage;
+	ULONG HighWaterNamePoolUsage;
+	ULONG HighWaterHandleTableUsage;
+	ULONG InvalidAttributes;
+	GENERIC_MAPPING GenericMapping;
+	ULONG ValidAccess;
+	BOOLEAN SecurityRequired;
+	BOOLEAN MaintainHandleCount;
+	USHORT MaintainTypeList;
+	POOL_TYPE PoolType;
+	ULONG PagedPoolUsage;
+	ULONG NonPagedPoolUsage;
+} OBJECT_TYPE_INFORMATION, *POBJECT_TYPE_INFORMATION;
+
+
+// 
+const FILE_INFORMATION_CLASS FileNameInformation =
+(FILE_INFORMATION_CLASS)9;
+const FILE_INFORMATION_CLASS FileAccessInformation =
+(FILE_INFORMATION_CLASS)8;
+
+//  
+// 
+// 
+typedef NTSTATUS(WINAPI * PFN_NTQUERYINFORMATIONFILE)(
+	IN HANDLE FileHandle,
+	OUT PIO_STATUS_BLOCK IoStatusBlock,
+	OUT PVOID FileInformation,
+	IN ULONG Length,
+	IN FILE_INFORMATION_CLASS FileInformationClass
+	);
+
+// 
+typedef struct _FILE_NAME_INFORMATION {
+	ULONG FileNameLength;
+	WCHAR FileName[1];
+} FILE_NAME_INFORMATION, *PFILE_NAME_INFORMATION;
+
+typedef struct _FILE_ACCESS_INFORMATION {
+	ACCESS_MASK AccessFlags;
+} FILE_ACCESS_INFORMATION, *PFILE_ACCESS_INFORMATION;
